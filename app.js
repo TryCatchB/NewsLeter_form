4d50653442b305a47a567b57c6dca5e3562e65e6
TryCatchB <pozdrakovzena@gmail.com>
2023-07-16 17:26:55 +0300

    NewsLeter_form

--- app.js
+++ app.js
@@ -1,18 +1,59 @@
-const $formInfo = document.querySelector(".form-info");
-const $inputFrom = $formInfo.querySelector(".form-info__input");
-const $buttonForm = $formInfo.querySelector(".form-info__button");
 const $popup = document.querySelector(".popup");
-const regex = /\w+@\w+\.com/g;
+const $formInfo = document.querySelector(".form-info");
+const $inputForm = $formInfo.querySelector(".form-info__input");
+const $formInfoHeaders = document.querySelector(".form-info__headers");
 
-$formInfo.addEventListener("submit", () => {
-  if (regex.test($inputFrom.value)) {
-    $popup.classList.add("active");
-  } else {
+$popup.addEventListener("click", hidePopup);
+
+$formInfo.addEventListener("submit", (event) => {
+  checkEmailUser(event);
+
+  displayErrorMessage();
+
+  if ($popup.classList.contains("active")) {
+    $inputForm.classList.remove("wrong");
+    $inputForm.value = "";
+    hideErrorMessage();
   }
 });
 
-$buttonForm.addEventListener("click", () => {
-  if (!regex.test($inputFrom.value)) {
-    $inputFrom.classList.add("wrong");
+function checkEmailUser(event) {
+  event.preventDefault();
+  const regex = /\w+@\w+\.com/g;
+
+  regex.test($inputForm.value)
+    ? $popup.classList.add("active")
+    : $inputForm.classList.add("wrong");
+}
+
+function displayErrorMessage() {
+  const $errorMessageElement = $formInfoHeaders.querySelector(
+    ".form-info__title-wrong"
+  );
+
+  if (!$errorMessageElement) {
+    $formInfoHeaders.insertAdjacentHTML(
+      "beforeend",
+      `<h5 class="form-info__title-wrong">Valid email required</h5>`
+    );
   }
-});
+}
+
+function hideErrorMessage() {
+  const $errorMessageElement = $formInfoHeaders.querySelector(
+    ".form-info__title-wrong"
+  );
+
+  if ($errorMessageElement) {
+    $errorMessageElement.remove();
+  }
+}
+
+function hidePopup(event) {
+  const currentTarget = event.target;
+  const $btnPopup = $popup.querySelector("button");
+
+  if (currentTarget === $btnPopup) {
+    $popup.classList.remove("active");
+  }
+}
